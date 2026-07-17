335847288b5267ae1d76d1ed067c338d8480181a
Kamaraj Perumal <kamaraj.perumal@perficient.com>
2026-07-17 09:41:11 +0530

    deployement deatils

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,39 +1,63 @@
-import { Component, OnInit, ChangeDetectionStrategy, signal, computed, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  signal,
+  computed,
+  inject,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
+
+const API_URL = 'http://localhost:5247/api/jobs';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
-
+export class Header implements OnInit {
+  constructor(private http: HttpClient) {}
   headerTitle = 'Zomota';
+
   store = inject(Store);
   valuenameValue = signal(this.functionCall());
   age = this.store.selectSignal((state) => state.zomotaData.age);
   dob = signal('1990-01-01');
 
-  isMajor = computed(() =>  this.age() >= 18);
+  isMajor = computed(() => this.age() >= 18);
 
   ngOnInit(): void {
-    this.valuenameValue.set("set value using set method");
+    this.valuenameValue.set('set value using set method');
+    this.loadJobs();
   }
-functionCall(){
-  console.log('function called');
-  return 'function called';
-}
+
+  loadJobs(): void {
+    this.http.get(API_URL).subscribe({
+      next: (response) => {
+        console.log('API Response:', response);
+      },
+      error: (error) => {
+        console.error('API Error:', error);
+      },
+    });
+  }
+
+  functionCall() {
+    console.log('function called');
+    return 'function called';
+  }
   buttonClick() {
     console.log('clicked');
     this.headerTitle = 'Zomota App';
     setTimeout(() => {
-     this.valuenameValue.update((v) => v+' updated value using update method');
+      this.valuenameValue.update((v) => v + ' updated value using update method');
     }, 2000);
   }
 
-  cumputedEvent(){
-  console.log('computed event called');
+  cumputedEvent() {
+    console.log('computed event called');
   }
 }
